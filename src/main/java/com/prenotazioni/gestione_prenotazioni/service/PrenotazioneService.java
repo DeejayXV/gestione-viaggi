06cbd0d51c16617f3420711be5200da4e8404db6
John <deejay.gvr@gmail.com>
2024-09-13 13:14:04 +0200

    service prenotazione

---
+++ src/main/java/com/prenotazioni/gestione_prenotazioni/service/PrenotazioneService.java
@@ -0,0 +1,27 @@
+package com.prenotazioni.gestione_prenotazioni.service;
+
+import com.prenotazioni.gestione_prenotazioni.entity.Prenotazione;
+import com.prenotazioni.gestione_prenotazioni.repository.PrenotazioneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+public class PrenotazioneService {
+
+    @Autowired
+    private PrenotazioneRepository prenotazioneRepository;
+
+    public Prenotazione creaPrenotazione(Prenotazione prenotazione) {
+        Optional<Prenotazione> esistente = prenotazioneRepository
+                .findByDipendenteIdAndDataRichiesta(prenotazione.getDipendente().getId(), prenotazione.getDataRichiesta());
+
+        if (esistente.isPresent()) {
+            throw new IllegalArgumentException("Esiste già una prenotazione per questo dipendente nella data selezionata.");
+        }
+
+        return prenotazioneRepository.save(prenotazione);
+    }
+}
