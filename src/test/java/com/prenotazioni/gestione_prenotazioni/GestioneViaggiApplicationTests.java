1b2ab4214bd32385239006aaf6afa2030703aef9
John <deejay.gvr@gmail.com>
2024-09-13 12:23:30 +0200

    Initial commit

---
+++ src/test/java/com/prenotazioni/gestione_prenotazioni/GestioneViaggiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prenotazioni.gestione_prenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneViaggiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
